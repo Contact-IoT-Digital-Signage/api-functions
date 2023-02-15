f50351b41550ed4de8ab16eabe916d436da68324
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-02-14 17:19:55 -0800

    add display_name, edit object_file_name

--- ai-processing/func.py
+++ ai-processing/func.py
@@ -8,30 +8,24 @@ import datetime
 def handler(ctx, data: io.BytesIO = None):
     event = json.loads(data.getvalue())
     object_name = event['data']['resourceName']
+    object_file_name = object_name.split('.')[0] 
 
     signer = oci.auth.signers.get_resource_principals_signer()
     media_flow = oci.media_services.MediaServicesClient({}, signer=signer)
 
-    media_workflow_id = os.environ.get('MEDIA_WORKFLOW_ID')
     compartment_id = os.environ.get('COMPARTMENT_ID')
-    date = datetime.datetime.now().strftime("%m-%d-%Y_%H%M")
-
-    sample = media_flow.list_media_workflow_configurations(compartment_id=compartment_id)
-    #logging.getLogger().info(sample.data.items)
-
+    media_workflow_id = os.environ.get('MEDIA_WORKFLOW_ID')
+    
     job_details = oci.media_services.models.CreateMediaWorkflowJobByIdDetails(
         compartment_id=compartment_id,
         workflow_identifier_type=oci.media_services.models.CreateMediaWorkflowJobDetails.WORKFLOW_IDENTIFIER_TYPE_ID,
         media_workflow_id=media_workflow_id,
+        display_name=object_file_name,
         parameters={
             "input": {
                 "objectName": object_name,
-                "objectNameFilename": object_name
+                "objectNameFilename": object_file_name
             },
-            #TODO: remove duplicates of output file name
-            # "output":{
-            #     "objectNameFilename": object_name+date,
-            # } 
         }
     )
     try:
@@ -40,4 +34,3 @@ def handler(ctx, data: io.BytesIO = None):
 
     except Exception as e:
         logging.getLogger().info(e)
-
